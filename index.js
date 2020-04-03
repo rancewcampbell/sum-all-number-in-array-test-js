d568007a5bdf4f2bae9310c29daae045bed15c7a
Rance Campbell <rancewcampbell@gmail.com>
2020-04-03 22:34:58 +0000

    completed sumItems recursive function

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  let sum = 0;
+  for (const item of array) {
+    Array.isArray(item) ? (sum += sumItems(item)) : (sum += item);
+  }
+  return sum;
 }
 
 module.exports = sumItems;
